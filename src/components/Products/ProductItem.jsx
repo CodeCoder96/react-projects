13fa1903f476e222065e6a092480bdf9f86463a1
furkan.yilmaz <yfurkan1996@gmail.com>
2024-05-21 10:23:57 +0300

    component içinde javascript yazma

--- src/components/Products/ProductItem.jsx
+++ src/components/Products/ProductItem.jsx
@@ -1,17 +1,18 @@
 import "./ProductItem.css";
 
 function ProductItem() {
+  const image = "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg";
+  const title = "Çanta";
+  const price = 500;
+
   return (
     <div className="product-item">
       <div className="product-image">
-        <img
-          src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
-          alt="product image"
-        />
+        <img src={image} alt="product image" />
       </div>
       <div className="product-info">
-        <strong>Title</strong>
-        <span>500₺</span>
+        <strong>{title}</strong>
+        <span>{price}</span>
       </div>
     </div>
   );
